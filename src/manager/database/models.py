a1b273dfde1807d4e47e342d64fbbc5a06625342
Blackprincex <appiahboaduprince@gmail.com>
2022-10-30 19:04:34 +0000

    chore: add database `tables`

---
+++ src/manager/database/models.py
@@ -0,0 +1,58 @@
+from sqlalchemy import Column, String, DateTime, Integer, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+BASE = declarative_base()
+
+
+# TODO establish a relationship between Events and Executives
+
+
+class ExecutiveEvent(BASE):
+    __tablename__ = "executive_event"
+
+    event_id = Column(ForeignKey("event.id"), primary_key=True)
+    host_id = Column(ForeignKey("user.id"), primary_key=True)
+
+    host_id = relationship("User", back_populates="executives", lazy="selectin")
+    event = relationship("Event", back_populates="events", lazy="selectin")
+
+
+class Event(BASE):
+    __tablename__ = "event"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    details = Column(String, nullable=False, unique=False)
+    guest = Column(String, nullable=True)
+    host = Column(String, nullable=False, unique=True)
+    dt_created = Column(DateTime, nullable=False)
+
+    events: list[ExecutiveEvent] = relationship(
+        "ExecutiveEvent",
+        back_populates="event",
+        lazy="selectin",
+    )
+
+
+class User(BASE):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False, unique=True)
+    bio = Column(String, nullable=True, unique=False)
+    is_executive = Column(Boolean, nullable=True)
+
+    executives: list[ExecutiveEvent] = relationship(
+        "ExecutiveEvent",
+        back_populates="user",
+        lazy="selectin",
+    )
+
+
+class Issues(BASE):
+    __tablename__ = "issues"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    statement = Column(String, nullable=False, unique=False)
+    dt_created = Column(DateTime, nullable=False)
+    dt_resolved = Column(DateTime, nullable=True)
+    is_resolved = Column(Boolean, nullable=False, default=False)
