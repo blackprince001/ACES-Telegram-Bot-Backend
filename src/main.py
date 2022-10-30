8c8fcde4ca08281e2b3a43ff9576c1ba154601c7
Blackprincex <appiahboaduprince@gmail.com>
2022-10-30 19:05:33 +0000

    chore: bind database in `main`

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+
+from src.manager.database.models import BASE
+from src.manager.database.core import engine
+
+BASE.metadata.create_all(bind=engine)
+
