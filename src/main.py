751b77243f04a45dd049b73aa813547bec03f539
Blackprincex <appiahboaduprince@gmail.com>
2022-11-02 19:51:35 +0000

    upd: resolved imports `main`

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
-
+from sqlalchemy.orm import Session
 from src.manager.database.models import BASE
 from src.manager.database.core import engine
 
