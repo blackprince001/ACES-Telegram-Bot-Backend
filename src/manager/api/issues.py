f6027e4590c6fec9674101bfca940dc2960d9f2d
Blackprincex <appiahboaduprince@gmail.com>
2022-11-03 19:06:40 +0000

    upd: added api `crud` methods to `issue` table

--- src/manager/api/issues.py
+++ src/manager/api/issues.py
@@ -1,22 +1,37 @@
 from sqlalchemy import select, update
 from sqlalchemy.orm import Session
-from manager.database.schemas.issues import IssueCreate as IssueSchema
-from src.manager.database.models import Issues as issuesModel
+from manager.database.schemas.issues import IssueCreate
+from src.manager.database.models import Issues as IssuesModel
+import datetime
 
-# TODO write issuess to database
-def create_issues():
-    pass
+def create_issues(db: Session, new_issue: IssueCreate) -> IssuesModel:
+    db_issue = IssuesModel(**new_issue.dict())
 
+    db.add(db_issue)
+    db.commit()
+    db.refresh(db_issue)
 
-def get_issues(db: Session, issues_id: int) -> issuesModel | None:
-    return db.get(issuesModel, issues_id)
-
-
-# TODO update Issue is_resolved column
-def issue_resolved():
-    pass
+    return db_issue
 
 
-# TODO update existing issue statement
-def update_issues_statement():
-    pass
+def get_issues(db: Session, issues_id: int) -> IssuesModel | None:
+    return db.get(IssuesModel, issues_id)
+
+
+def set_issue_resolved(db: Session, issue_id: int, date: datetime, state: bool):
+    if state:
+        return db.execute(
+            update(IssuesModel)
+            .where(IssuesModel.id == issue_id)
+            .values(is_resolved=True, dt_resolved=date)
+        )
+
+    return get_issues(db=db, issues_id=issue_id)
+
+
+def update_issues_statement(db: Session, issue_id: int, new_statement: str):
+    return db.execute(
+        update(IssuesModel)
+        .where(IssuesModel.id == issue_id)
+        .values(statement=new_statement)
+    )
