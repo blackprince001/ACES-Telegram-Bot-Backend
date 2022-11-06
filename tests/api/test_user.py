cf5458e2463b46d028156c4d9037b28f0281a1ef
Blackprincex <appiahboaduprince@gmail.com>
2022-11-06 17:08:00 +0000

    fix: fixture functions improved

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -15,12 +15,13 @@ def test_create_user(db, user):
 
     assert db_user.name == "blackprince"
     assert db_user.bio is not None
-    assert db_user.id == 1
+    assert db_user.id
     assert db_user.is_executive is False
 
 
-def test_get_users(db):
-    db_user = get_user(db=db, user_id=1)
+def test_get_users(db, user):
+    entry = create_user(db=db, new_user=user)
+    db_user = get_user(db=db, user_id=entry.id)
     db_users = get_users(db=db)
 
     assert db_user.name == "blackprince"
@@ -36,32 +37,34 @@ def test_create_executive(db, executive):
     assert db_executive.is_executive is True
 
 
-def test_get_executives(db):
-    executive = get_executive(db=db, user_id=2)
+def test_get_executives(db, executive):
+    entry = create_user(db=db, new_user=executive)
+    executive = get_executive(db=db, user_id=entry.id)
     executives = get_executives(db=db)
 
     assert executive.name == "Trump"
     assert executives != []
 
 
-def test_update_user_bio(db):
+def test_update_user_bio(db, user):
+    entry = create_user(db=db, new_user=user)
     new_bio = """The problem is that you are not adequate, the problem is, you think you should be."""
-    db_user = update_user_bio(db=db, new_bio=new_bio)
+    db_user = update_user_bio(db=db, new_bio=new_bio, user_id=user.id)
 
     assert db_user.bio == new_bio
     assert db_user.name == "blackprince"
 
 
-def test_update_user_name(db):
+def test_update_user_name(db, user):
     new_name = "Black King Kong"
-    db_user = update_user_name(db=db, user_id=1, new_name=new_name)
+    db_user = update_user_name(db=db, user_id=user.id, new_name=new_name)
 
     assert db_user.name == new_name
     assert db_user.is_executive is False
 
 
-def test_update_executive_status(db):
-    db_executive = update_executive_status(db=db, user_id=2)
+def test_update_executive_status(db, executive):
+    db_executive = update_executive_status(db=db, user_id=executive.id)
 
     assert db_executive.is_executive is False
     assert db_executive.name == "Trump"
