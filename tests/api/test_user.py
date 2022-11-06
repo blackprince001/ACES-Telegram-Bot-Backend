3fb690ba830a30f03821425ad9cd1d10fb32bcb0
Blackprincex <appiahboaduprince@gmail.com>
2022-11-06 17:08:23 +0000

    fix: more fixes to pytest fixtures

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -49,15 +49,16 @@ def test_get_executives(db, executive):
 def test_update_user_bio(db, user):
     entry = create_user(db=db, new_user=user)
     new_bio = """The problem is that you are not adequate, the problem is, you think you should be."""
-    db_user = update_user_bio(db=db, new_bio=new_bio, user_id=user.id)
+    db_user = update_user_bio(db=db, new_bio=new_bio, user_id=entry.id)
 
     assert db_user.bio == new_bio
     assert db_user.name == "blackprince"
 
 
 def test_update_user_name(db, user):
+    entry = create_user(db=db, new_user=user)
     new_name = "Black King Kong"
-    db_user = update_user_name(db=db, user_id=user.id, new_name=new_name)
+    db_user = update_user_name(db=db, user_id=entry.id, new_name=new_name)
 
     assert db_user.name == new_name
     assert db_user.is_executive is False
